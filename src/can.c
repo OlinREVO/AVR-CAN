25aa205797cd0ff980ac90b0d73df84092e411a8
Derek Redfern <redfern.derek@gmail.com>
2013-03-16 11:50:51 -0400

    Finished send, wrote receive initializer, fixed syntax bugs

--- src/can.c
+++ src/can.c
@@ -11,19 +11,34 @@
 
 
 int initCan () {
-    // enable interrupts: all, receive, transmit
+    // enable interrupts: all, receive
     CANGIE = (_BV(ENIT) | _BV(ENRX));
     CANIE1 = 0; // compatibility with future chips
     CANIE2 = (_BV(IEMOB0) | _BV(IEMOB1)); // enable interrupts on first two MObs
+
+    // set up the first MOb for reception
     CANPAGE = 0; // first MOb
-    CANCDMOB = _BV(CONMOB1); // enable reception
-    CANPAGE = _BV(MOBNB0); // first MOb
-    CANCDMOB = _BV(CONMOB1); // enable reception
+    // set compatibility registers, RTR bit, and reserved bit to 0
+    CANIDT4 = 0;
+    CANIDT3 = 0;
+    // set desired ID tag to receive
+    CANIDT2 = 0b10100000; // last 5 bits must be 0
+    CANIDT1 = 0b11001100;
+    // set compatibility registers to 0, RTR/IDE-mask to 1
+    CANIDM4 = (_BV(RTRMSK) | _BV(IDEMSK));
+    CANIDM3 = 0;
+    // only accept one specific message ID
+    CANIDM2 = (_BV(IDMSK2) | _BV(IDMSK1) | _BV(IDMSK0));
+    CANIDM1 = 1;
+    // enable reception
+    CANCDMOB = _BV(CONMOB1); 
+
     // TODO: set ID tags and masks for msgs this MOb can accept
     return(0);
 }
 
 // handle CAN interrupt
+// for testing purposes, assuming this is an RX interrupt
 int ISR(CANIT_vect) {
     char cSREG = SREG; //store SREG
     CANSTMOB |= _BV(RXOK); // reset receive interrupt flag
@@ -39,11 +54,8 @@ int ISR(CANIT_vect) {
         data[i] = CANMSG;
 
         // display received data on LEDs
-        int j;
-        for (j = 0; j < 8; ++j) {
-            PORTB = data[i][j];
-            _delay_loop_2(30000)
-        }
+        PORTB = data[i];
+        _delay_loop_2(30000);
     }
     free(data);
     SREG=cSREG; //restore SREG
@@ -51,22 +63,24 @@ int ISR(CANIT_vect) {
 }
 
 int sendCANMsg() {
-    uint8_t data = 0b00011011; // test data
+    uint8_t data = 0b00011011; // test msg
     uint8_t dataLength = 1; // only sending one test byte
     CANPAGE = (_BV(MOBNB0) | _BV(AINC)); // set MOb number (1 for testing) and auto-increment bits in CAN page MOb register
-    CANCDMOB = (_BV(CONMOB0) | dataLength); // set transmit bit and data length bits of MOb control register
     CANMSG = data; // set data page register
-    // set extraneous/compatibility registers to 0
+
+    // set compatibility registers, RTR bit, and reserved bit to 0
     CANIDT4 = 0;
     CANIDT3 = 0;
-    // set ID tag registers
-    CANIDT2 = 0; //TODO
-    CANIDT1 = 0; //TODO
 
+    // set ID tag registers
+    CANIDT2 = 0b10100000; // last 5 bits must be 0
+    CANIDT1 = 0b11001100;
+
+    CANCDMOB = (_BV(CONMOB0) | dataLength); // set transmit bit and data length bits of MOb control register
     return(0);
 }
 
-int main () {
+int main (void) {
     unsigned int i;
     // set all PORTB pins for output
     DDRB = 0xFF;
