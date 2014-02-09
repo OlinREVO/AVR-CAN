c5adb944ff072d07297b7b7ab9fd69a31dc6a02d
Derek Redfern <derek@quanttus.com>
2014-02-09 15:49:41 -0500

    Allow multiple LEDs to be on at the same time

--- src/nodetemp2.c
+++ src/nodetemp2.c
@@ -47,14 +47,6 @@ ISR(INT3_vect) {
 // TODO: change this method for each of the demo nodes
 void handleCANmsg(uint8_t destID, uint8_t msgID, uint8_t* msg, uint8_t msgLen) {
     uint8_t cmd = msg[0];
-    //Turn both off first
-/*    if (destID == NODE_HOME) {
-        PORTD |= _BV(PD3);
-    } else {
-        PORTD &= ~(_BV(PD3));
-    }*/
-    PORTB &= ~(_BV(PB4));
-    PORTB &= ~(_BV(PB6));
     int ledOn = cmd & 0b01;
     int whichLED = cmd & 0b10;
     if (whichLED) {
